d41afd947911b005e64541693ba0808ea98b4494
HSD\aanderson <aanderson@hollandspecial.com>
2018-01-04 11:42:43 -0500

    put firebase data for clients into table

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ClientService } from '../../services/client.service';
-import { Client } from '../../models/client';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,16 +6,12 @@ import { Client } from '../../models/client';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  clients: any[];
-
-  constructor(private clientService: ClientService) {
+  constructor() {
 
    }
 
   ngOnInit() {
-    this.clientService.getClients().subscribe(clients => {
-      console.log(clients);
-    });
+
   }
 
 }

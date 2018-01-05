deed7e95fc725702cdfd552caab8251167bbd42c
HSD\aanderson <aanderson@hollandspecial.com>
2018-01-05 14:17:08 -0500

    added firebase data to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDYjf_Pc8eKnvUBHOGpAMFd6-CC_8yQsuw',
+    authDomain: 'clientpanelapp-db814.firebaseapp.com',
+    databaseURL: 'https://clientpanelapp-db814.firebaseio.com',
+    projectId: 'clientpanelapp-db814',
+    storageBucket: 'clientpanelapp-db814.appspot.com',
+    messagingSenderId: '820911947760'
+  }
 };
